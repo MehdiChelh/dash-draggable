1adfd8b90bad1d074a10bb13cba95da457effb84
Mehdi Echchelh <mehdi.echel@gmail.com>
2021-04-19 13:33:06 +0200

    :memo: [UPDATE] version

--- src/DashDraggable.jl
+++ src/DashDraggable.jl
@@ -3,7 +3,7 @@ module DashDraggable
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1"
+const version = "0.1.1a"
 
 include("''_dashboarditem.jl")
 include("''_dashboarditemresponsive.jl")
